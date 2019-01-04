eb598305faa9be136a855dc7871a5ab9dd3e7767
dihiddie <zagrtd@gmail.com>
2019-01-04 18:14:23 +0300

    create or edit page added

--- WebApplication1/Models/Diary/PostViewModel.cs
+++ WebApplication1/Models/Diary/PostViewModel.cs
@@ -1,12 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace dihiddieDiary.Models.Diary
 {
     public sealed class PostViewModel
     {
+        public int Id { get; set; }
 
+        [Display(Name = "Заголовок")]
+        public string Title { get; set; }
+
+        public string Content { get; set; }
     }
 }
